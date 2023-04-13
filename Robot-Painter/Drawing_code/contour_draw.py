8cd0879510dc80192fc15fee072c37b72535de75
Egor-Magazenkov <egormaga04@gmail.com>
2023-04-13 16:06:52 +0300

    Pylint checked + Minor fixes + modified README

--- Robot-Painter/Drawing_code/contour_draw.py
+++ Robot-Painter/Drawing_code/contour_draw.py
@@ -1,128 +1,137 @@
-import json
+"""Code for extracting features from image in bezier curves"""
+from tkinter import filedialog, Tk
+import pickle
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button
 from matplotlib.patches import Rectangle
-from tkinter import filedialog, Tk
 import roboticstoolbox.tools.trajectory as rtb
-import pickle
 
 
-# find the a & b points
 def get_bezier_coef(points):
-        # since the formulas work given that we have n+1 points
-        # then n must be this:
-        n = len(points) - 1
+    """Connect ordered set of points with C2 continuous bezier curves."""
+    # since the formulas work given that we have n+1 points
+    # then n must be this:
+    n_points = len(points) - 1
 
-        # build coefficents matrix
-        C = 4 * np.identity(n)
-        np.fill_diagonal(C[1:], 1)
-        np.fill_diagonal(C[:, 1:], 1)
-        C[0, 0] = 2
-        C[n - 1, n - 1] = 7
-        C[n - 1, n - 2] = 2
+    # build coefficents matrix
+    coeffs = 4 * np.identity(n_points)
+    np.fill_diagonal(coeffs[1:], 1)
+    np.fill_diagonal(coeffs[:, 1:], 1)
+    coeffs[0, 0] = 2
+    coeffs[n_points - 1, n_points - 1] = 7
+    coeffs[n_points - 1, n_points - 2] = 2
 
-        # build points vector
-        P = [2 * (2 * points[i] + points[i + 1]) for i in range(n)]
-        P[0] = points[0] + 2 * points[1]
-        P[n - 1] = 8 * points[n - 1] + points[n]
+    # build points vector
+    res_points = [2 * (2 * points[i] + points[i + 1]) for i in range(n_points)]
+    res_points[0] = points[0] + 2 * points[1]
+    res_points[n_points - 1] = 8 * points[n_points - 1] + points[n_points]
 
-        # solve system, find a & b
-        A = np.linalg.solve(C, P)
-        B = [0] * n
-        for i in range(n - 1):
-            B[i] = 2 * points[i + 1] - A[i + 1]
-        B[n - 1] = (A[n - 1] + points[n]) / 2
+    # solve system, find a & b
+    control_point_1 = np.linalg.solve(coeffs, res_points)
+    control_point_2 = [0] * n_points
+    for i in range(n_points - 1):
+        control_point_2[i] = 2 * points[i + 1] - control_point_1[i + 1]
+    control_point_2[n_points - 1] = (control_point_1[n_points - 1] + points[n_points]) / 2
 
-        return points[:-1], A, B, points[1:]
-# evalute each cubic curve on the range [0, 1] sliced in n points
-def evaluate_bezier(points, n, i):
+    return points[:-1], control_point_1, control_point_2, points[1:]
+
+def evaluate_bezier(points, n_points, i):
+    """Generate sets of n_points points from cubic bezier coeffitions."""
     # returns the general Bezier cubic formula given 4 control points
-    def get_cubic(a, b, c, d):
-        return lambda t: np.power(1 - t, 3) * a + 3 * np.power(1 - t, 2) * t * b + 3 * (1 - t) * np.power(t, 2) * c + np.power(t, 3) * d
+    def get_cubic(cp_1, cp_2, cp_3, cp_4):
+        return lambda t: \
+                np.power(1 - t, 3) * cp_1 + \
+                3 * np.power(1 - t, 2) * t * cp_2 + \
+                3 * (1 - t) * np.power(t, 2) * cp_3 + \
+                np.power(t, 3) * cp_4
 
     # return one cubic curve for each consecutive points
     def get_bezier_cubic(points, i):
-        _, A, B,_ = get_bezier_coef(points)
+        _, control_point_1, control_point_2, _ = get_bezier_coef(points)
         return [
-            get_cubic(points[i], A[i], B[i], points[i + 1])
+            get_cubic(points[i], control_point_1[i], control_point_2[i], points[i + 1])
             for i in range(len(points) - 1)
         ]
 
     curves = get_bezier_cubic(points, i)
-    return np.array([fun(t) for fun in curves for t in np.linspace(0, 1, n)])
+    return np.array([fun(t) for fun in curves for t in np.linspace(0, 1, n_points)])
 
 def convert_path(path):
-    res = path/compression_coeff
+    """Convert path to fit picture on canvas."""
+    res = path/COMPRESSION_COEFF
     # flip image
-    res[:,1] = canvas_size - res[:,1]%canvas_size
-    res = np.round(res/2, 1)*2
+    res[:, 1] = CANVAS_SIZE - res[:, 1] % CANVAS_SIZE
+    res = np.round(res/2, 1) * 2
     res = np.array([res[i] for i in sorted(np.unique(res, return_index=True, axis=0)[1])])
     return res
-    
-def draw_paths(paths):
-    canvas.add_patch(Rectangle((0, 0), canvas_size, canvas_size, fill = False))
+
+def draw_paths(points):
+    """Plot points onto the canvas."""
+    canvas.add_patch(Rectangle((0, 0), CANVAS_SIZE, CANVAS_SIZE, fill = False))
     cnt = 0
-    for path in paths:
+    for path in points:
         path = convert_path(path)
         canvas.plot(path[:,0], path[:,1], linewidth=1, color='black')
         cnt += len(path)
-    print(cnt)
     fig.canvas.draw()
 
-
-
-def image_processing(img,  smooth=95, scale=192, blur=5, sigma=5):
-    def image_transform(img, smooth=95, scale=192):
-        smooth = cv2.GaussianBlur(img, (smooth,smooth), 0)
-        division = cv2.divide(img, smooth, scale=250)
+def image_processing(src,  smooth=95, scale=192, blur=5, sigma=5):
+    """Preprocess image to get rid of glitches, shadows and noises."""
+    def image_transform(src, smooth=smooth, scale=scale):
+        smooth = cv2.GaussianBlur(src, (smooth,smooth), 0)
+        division = cv2.divide(src, smooth, scale=scale)
         # cv2.imshow('s', division)
         # cv2.waitKey(0)
         return division
-    
-    def adjust_brightness(img):
-        cols, rows = img.shape
-        brightness = np.sum(img) / (255 * cols * rows)
+
+    def adjust_brightness(src):
+        cols, rows = src.shape
+        brightness = np.sum(src) / (255 * cols * rows)
         min_br = 0.8
         ratio = brightness / min_br
         if ratio >= 1:
             print("Image already bright enough")
-            return img
-        return cv2.convertScaleAbs(img, alpha = 1 / ratio, beta = 0)
+            return src
+        return cv2.convertScaleAbs(src, alpha = 1 / ratio, beta = 0)
 
-    img = image_transform(img)
-    img = adjust_brightness(img)
-    img = cv2.GaussianBlur(img, (blur,blur), sigma)
-    return img
+    res = image_transform(src)
+    res = adjust_brightness(res)
+    res = cv2.GaussianBlur(res, (blur,blur), sigma)
+    return res
 
 
 
-canvas_size = 400
+CANVAS_SIZE = 400
 paths = []
-filepath = ''
-filename = ''
-compression_coeff = None
-img = None
-img_width = None
-canvas = None
+FILEPATH = ''
+FILENAME = ''
+COMPRESSION_COEFF = None
+IMAGE = None
+IMG_WIDTH = None
 
 
-def img_to_square(img):
-    x=y=max(img.shape[0], img.shape[1])
-    square= np.ones((x,y), np.uint8)*255
-    square[int((y-img.shape[0])/2):int(y-(y-img.shape[0])/2), int((x-img.shape[1])/2):int(x-(x-img.shape[1])/2)] = img
-    square = cv2.resize(square, (400, 400))
+def img_to_square(src):
+    """Refactor image to square size with bigger side as side of square."""
+    height = width = max(src.shape[0], src.shape[1])
+    square= np.ones((height,width), np.uint8)*255
+    square[
+            int((width-src.shape[0])/2):int(width-(width-src.shape[0])/2),
+            int((height-src.shape[1])/2):int(height-(height-src.shape[1])/2)
+            ] = src
+    square = cv2.resize(square, (CANVAS_SIZE, CANVAS_SIZE))
     return square
 
 
 def start():
-    global img_width, compression_coeff, img, gabor_res
-    img = img_to_square(cv2.imread(filepath+filename, 0))
-    # img = sharpening(img)
-    img_width = img.shape[0]
-    compression_coeff = img_width/canvas_size
-    print(compression_coeff)
+    """Begin the image processing algos."""
+    global IMG_WIDTH, COMPRESSION_COEFF, IMAGE
+    IMAGE = img_to_square(cv2.imread(FILEPATH+FILENAME, 0))
+    # IMAGE = sharpening(IMAGE)
+    IMG_WIDTH = IMAGE.shape[0]
+    COMPRESSION_COEFF = IMG_WIDTH/CANVAS_SIZE
+    print(f'Compressing the image {COMPRESSION_COEFF} times')
     update(100)
 
 fig, canvas = plt.subplots(figsize = (10, 10))
@@ -146,52 +155,48 @@ blur_slider = Slider(blur_axes, "Blur", 1, 9, 5, valstep = 2)
 sigma_axes = plt.axes([0.25, 0.3, 0.5, 0.02])
 sigma_slider = Slider(sigma_axes, "Sigma", 1, 200, 5, valstep = 2)
 
-def update(val):
-    global paths
-    # fig.clf()
-    canvas.cla()
-    result=[]
-    # result += gabor_res
-    
-    canny_val1 = canny_slider1.val
-    canny_val2 = canny_slider2.val
-    length = length_slider.val
-    ksize = ksize_slider.val
-    lambd = lambda_slider.val
-    blur = blur_slider.val
-    sigma = sigma_slider.val
+def gabor_filter(ksize, sigma, lambda_, length):
+    """Extract features from image using Gabor filter."""
+    img1 = cv2.filter2D(IMAGE, cv2.CV_8UC3,
+                        cv2.getGaborKernel((ksize, ksize), sigma, 0, lambda_, 0.5, np.pi))
+    img2 = cv2.filter2D(IMAGE, cv2.CV_8UC3,
+                        cv2.getGaborKernel((ksize, ksize), sigma, np.pi/2, lambda_, 0.5, np.pi))
+    img3 = cv2.filter2D(IMAGE, cv2.CV_8UC3,
+                        cv2.getGaborKernel((ksize, ksize), sigma, np.pi/4, lambda_, 0.5, np.pi))
+    img4 = cv2.filter2D(IMAGE, cv2.CV_8UC3,
+                        cv2.getGaborKernel((ksize, ksize), sigma, 3*np.pi/4, lambda_, 0.5, np.pi))
 
-    g_kernel_1 = cv2.getGaborKernel((ksize, ksize), sigma, 0, lambd, 0.5, np.pi)
-    g_kernel_2 = cv2.getGaborKernel((ksize, ksize), sigma, np.pi/2, lambd, 0.5, np.pi)
-    g_kernel_3 = cv2.getGaborKernel((ksize, ksize), sigma, np.pi/4, lambd, 0.5, np.pi)
-    g_kernel_4 = cv2.getGaborKernel((ksize, ksize), sigma, 3*np.pi/4, lambd, 0.5, np.pi)
-
-    img1 = cv2.filter2D(img, cv2.CV_8UC3, g_kernel_1)
-    img2 = cv2.filter2D(img, cv2.CV_8UC3, g_kernel_2)
-    img3 = cv2.filter2D(img, cv2.CV_8UC3, g_kernel_3)
-    img4 = cv2.filter2D(img, cv2.CV_8UC3, g_kernel_4)
-
-    img12 = cv2.bitwise_xor(img2,img1)
-    img34 = cv2.bitwise_xor(img3,img4)
-    img1234 = cv2.bitwise_xor(img12, img34)
+    img1234 = cv2.bitwise_xor(cv2.bitwise_xor(img2,img1), cv2.bitwise_xor(img3,img4))
     thinned = cv2.ximgproc.thinning(img1234)
-    res,hier = cv2.findContours(thinned, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    res, _ = cv2.findContours(thinned, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
     gabor_res = []
     for cnt in res:
         if cv2.arcLength(cnt, True) < length:
             continue
-        c = cv2.approxPolyDP(cnt, 10**(-10)*cv2.arcLength(cnt, True), True)
+        contour = cv2.approxPolyDP(cnt, 10**(-10)*cv2.arcLength(cnt, True), True)
         # c = np.array([c[i] for i in range(len(c)) if np.linalg.norm(c[i-1]-c[i]) > 2])
-        if len(c) >0:
-            gabor_res.append(c)
+        if len(contour) > 0:
+            gabor_res.append(contour)
     gabor_res = [np.array([point[0] for point in cnt]) for cnt in gabor_res]
-    result += gabor_res
-    image = image_processing(img.copy(), blur=blur)
+    return gabor_res
 
-    
-    canny = cv2.Canny(image, canny_val1, canny_val2, apertureSize=3, L2gradient = True)
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(1,1))
-    canny = cv2.morphologyEx(canny, cv2.MORPH_CLOSE, kernel)
+def update(val):
+    """Read the values from sliders, apply all algos and draw result onto canvas."""
+    global paths
+    # fig.clf()
+    canvas.cla()
+    result=[]
+
+    length = length_slider.val
+
+    result += gabor_filter(ksize_slider.val, sigma_slider.val, lambda_slider.val, length)
+
+    image = image_processing(IMAGE.copy(), blur=blur_slider.val)
+
+    canny = cv2.Canny(image, canny_slider1.val, canny_slider2.val,
+                                apertureSize=3, L2gradient = True)
+    canny = cv2.morphologyEx(canny, cv2.MORPH_CLOSE,
+                    cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(1,1)))
 
     contours, hierarchy = cv2.findContours(canny, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
     contours = list(contours)
@@ -204,7 +209,7 @@ def update(val):
 
         if i in cnts_to_del:
             continue
-        
+
         if hierarchy[i][2] != -1:
             cnts_to_del.append(hierarchy[i][2])
             k = hierarchy[hierarchy[i][2]][0]
@@ -212,14 +217,14 @@ def update(val):
                 cnts_to_del.append(k)
                 k = hierarchy[k][0]
 
-        c = cv2.approxPolyDP(cnt, 10**(-10)*cv2.arcLength(cnt, True), True)
+        contour = cv2.approxPolyDP(cnt, 10**(-10)*cv2.arcLength(cnt, True), True)
         # c = np.array([c[i] for i in range(len(c)) if np.linalg.norm(c[i-1]-c[i]) > 2])
-        c = [point[0] for point in c]
-        if not c:
+        contour = [point[0] for point in contour]
+        if not contour:
             continue
         # c.append(c[0])
-        c = np.array(c)
-        result.append(c)
+        contour = np.array(contour)
+        result.append(contour)
     draw_paths(result)
     paths=result
 
@@ -233,22 +238,26 @@ sigma_slider.on_changed(update)
 
 open_button_axes = plt.axes([0.2, 0.2, 0.2, 0.1])
 open_button = Button(open_button_axes, 'Открыть',color="cyan")
+
 def open_file(val):
-    global filename, filepath
+    """Handle the name of the file from dialog input."""
+    global FILENAME, FILEPATH
     Tk().withdraw()
     path_to_file = filedialog.askopenfilename(title = "Выберите фото",\
         filetypes=(("Фотографии", "*.jpg"), ("Фотографии", "*.png"), ("Фотографии", "*.jpeg")))
-    filename = path_to_file.split('/')[-1]
-    filepath = path_to_file.split(filename)[0]
+    FILENAME = path_to_file.split('/')[-1]
+    FILEPATH = path_to_file.split(FILENAME)[0]
     start()
 open_button.on_clicked(open_file)
 
 
 submit_button_axes = plt.axes([0.6, 0.2, 0.2, 0.1])
 submit_button = Button(submit_button_axes, 'Сохранить',color="green")
-def submit(val): 
-    fig.savefig(filepath + filename.split('.')[0] + 'res.png')       
-    with open(filepath + filename.split('.')[0] + '.txt', 'w') as file:
+
+def submit(val):
+    """Save the result canvas, trajectories and  sliders' values on triiger of SUBMIT button."""
+    fig.savefig(FILEPATH + FILENAME.split('.')[0] + 'res.png')
+    with open(FILEPATH + FILENAME.split('.')[0] + '.txt', 'w',encoding="utf8") as file:
         canny_val1 = canny_slider1.val
         canny_val2 = canny_slider2.val
         length = length_slider.val
@@ -263,18 +272,23 @@ def submit(val):
         file.write("ksize\t"+ str(ksize)+ "\n")
         file.write("sigma\t"+ str(sigma)+ "\n")
         file.write("lambda\t"+ str(lambd)+ "\n")
-    result = {
-    'trajectories':[]
-    }
+    with open('./trjs.pickle', 'rb') as file:
+        result = pickle.load(file)
+        for i, trj in reversed(list(enumerate(result['trajectories']))):
+            if trj['color'] == 0:
+                result['trajectories'].pop(i)
+            if trj['color'] != 0:
+                break
     def to_pickle(path):
-        path=convert_path(path)/1000
-        path_array = rtb.mstraj(path, dt=0.002, qdmax=0.25, tacc=0.05)
-        trj = {'points': path_array.q, 'width': 1.0, 'color': (0,0,0)}
+        path=convert_path(path)
+        path[:, 1] = CANVAS_SIZE - path[:, 1] % CANVAS_SIZE
+        path_array = rtb.mstraj(path/1000, dt=0.002, qdmax=0.25, tacc=0.05)
+        trj = {'points': path_array.q, 'width': 1.0, 'color': 0}
         return trj
     for path in paths:
         result['trajectories'].append(to_pickle(path))
-    
-    with open('./contour_trjs.pickle', 'wb') as file:
+
+    with open('./trjs.pickle', 'wb') as file:
         pickle.dump(result, file)
 submit_button.on_clicked(submit)
 
---
+++ Robot-Painter/Drawing_code/fill_areas.py
@@ -0,0 +1,162 @@
+"""Algorithm for spiral filling areas."""
+import sys
+import pickle
+import numpy as np
+import cv2
+import roboticstoolbox.tools.trajectory as rtb
+
+MIN_CONTOUR_AREA = 7
+
+data = {
+    'trajectories':[]
+}
+result_path = np.empty((0, 1, 2))
+
+def img_to_square(src):
+    """Refactor image to square size with bigger side as side of square."""
+    height = width = max(src.shape[0], src.shape[1])
+    if len(src.shape) == 3:
+        square= np.ones((height, width, 3))*(255,255,255)
+    else:
+        square= np.zeros((height, width))
+    square[int((width-src.shape[0])/2):int(width-(width-src.shape[0])/2),
+            int((height-src.shape[1])/2):int(height-(height-src.shape[1])/2)] = src
+    return square
+
+if len(sys.argv) != 5:
+    print('Bad argument usage.\n'
+                 'Usage: python3 fill_areas.py ' 
+                                '<path/to/image> '
+                                '<path/to/quantized_image> '
+                                '<path/to/segments.txt> '
+                                '<path/to/colors.txt> ')
+    sys.exit(1)
+else:
+    img = img_to_square(cv2.imread(sys.argv[1]))
+    quantized_img = img_to_square(cv2.imread(sys.argv[2]))
+    final_labels = img_to_square(np.loadtxt(sys.argv[3]))
+    COLORS = np.loadtxt(sys.argv[4], np.uint0)
+
+compression_coeff = max(img.shape[0], img.shape[1])/400
+
+result = np.zeros(img.shape, dtype=np.uint8)
+cv2.imshow('quantized_img', quantized_img.astype(np.uint8))
+
+def save_to_file(cnt, color, c_coeff=compression_coeff):
+    """Convert path to fit picture on canvas and put it into result dictionary."""
+    global data
+    cnt = np.array([point[0] for point in cnt])
+    trajectory = cnt.copy() / c_coeff
+    if len(trajectory) == 0:
+        return
+    #trajectory[:, 0] = 400 - trajectory[:, 0] - 1
+    trajectory /= 1000.0
+    # trj = {'points': trajectory, 'width': 1.0, 'color': np.where(COLORS==color)[0][0]}
+
+    trj_array = rtb.mstraj(trajectory, dt=0.02, qdmax=0.25, tacc=0.05)
+
+    trj = {'points': trj_array.q, 'width': 1.0, 'color': np.where(COLORS==color)[0][0]}
+    data['trajectories'].append(trj)
+
+def find_nearest_point(control_point, cnt):
+    """Get index of the nearest point of contour to control_point."""
+    min_ = np.inf
+    index = -1
+    for i, point in enumerate(cnt):
+        error = np.linalg.norm(control_point - point, 2)
+        if min_ > error:
+            index = i
+            min_ = error
+
+    if min_ >= 10:
+        return -1
+
+    return index
+
+def fill(mask_image, boundary, color, brush=3):
+    """Generate spiral or spirals to fill area bounded by border."""
+    global result_path
+    # result =np.zeros(img.shape)
+
+    if cv2.contourArea(boundary) < MIN_CONTOUR_AREA:
+        return
+
+    # print(result_path.shape)
+    if result_path.shape[0] != 0:
+        starting_index = find_nearest_point(result_path[-1][0], boundary)
+        if starting_index == -1:
+            save_to_file(result_path, color)
+            result_path = boundary
+        else:
+            boundary = np.roll(boundary, -starting_index, axis=0)
+            result_path = np.concatenate((result_path, boundary), axis=0)
+    else:
+        result_path = boundary
+
+    for point in boundary:
+        point = point[0]
+        for radius in range(-brush+1, brush):
+            try:
+                # if point + np.array([0,r]) in int_mask:
+                result[point[1]+radius][point[0]] = (color[0], color[1], color[2])
+                mask_image[point[1] + radius][point[0]] = 0
+            except IndexError:
+                pass
+                # print("BORDER WARNING")
+            try:
+                # if point + np.array([r,0]) in int_mask:
+                result[point[1]][point[0]+radius] = (color[0], color[1], color[2])
+                mask_image[point[1]][point[0] + radius] = 0
+            except IndexError:
+                pass
+                # print("BORDER WARNING")
+            try:
+                # if point + np.array([r,r]) in int_mask:
+                result[point[1]+radius][point[0]+radius] = (color[0], color[1], color[2])
+                mask_image[point[1] + radius][point[0] + radius] = 0
+            except IndexError:
+                pass
+                # print("BORDER WARNING")
+        cv2.imshow('result', result)
+        # cv2.imshow('amsk', mask_image)
+
+        # cv2.waitKey(1)
+
+    contours, _ = cv2.findContours(mask_image, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
+    # print(f'[INFO]:\t Found {len(contours)} contours')
+    # cv2.drawContours(mask_cnt, contours[0], -1, 255, thickness=-1)
+
+    for _, cnt in enumerate(contours):
+        if cv2.contourArea(cnt) < MIN_CONTOUR_AREA:
+            continue
+        # print(f"[INF^O]:\t Filling {i} contour")
+        mask_image  = np.zeros(mask_image.shape, dtype=np.uint8)
+        mask_image = cv2.drawContours(mask_image, [cnt], -1, 255, thickness=cv2.FILLED)
+        fill(mask_image, cnt, color, brush=brush)
+        cv2.waitKey(1)
+    save_to_file(result_path, color)
+    result_path = np.empty((0, 1, 2))
+
+
+for l in np.unique(final_labels.flatten()):
+    if l == 0:
+        continue
+    mask = np.ones((quantized_img.shape[0], quantized_img.shape[1]), np.uint8) * 255
+    mask[final_labels!=l] = 0
+    border, _ = cv2.findContours(mask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
+    border = border[0]
+    region_colors = np.array([quantized_img[i][j] for i,j in
+        zip(np.where(final_labels==l)[0], np.where(final_labels==l)[1])])
+    clrs, indexes, counts = np.unique(region_colors, axis=0, return_index=True, return_counts=True)
+    color_index = indexes[np.argmax(counts)]
+    color_ = region_colors[color_index]
+    # color_ = quantized_img[border[0][0][1]][border[0][0][0]]
+    result_path = np.empty((0, 1, 2))
+    fill(mask, border, color_)
+
+data['trajectories'].sort(key = lambda x: x['color'])
+print(len(data['trajectories']))
+with open('./trjs.pickle', 'wb') as file:
+    pickle.dump(data, file)
+cv2.imwrite('result_filled.png', result)
+cv2.waitKey(0)
